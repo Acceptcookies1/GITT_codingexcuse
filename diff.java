b841aa02bebd4d8fe1df3b5a24ec858db8f14f03
Acceptcookies1 <125445079+Acceptcookies1@users.noreply.github.com>
2023-03-27 00:58:10 +0530

    Update diff.java

--- diff.java
+++ diff.java
@@ -1,5 +1,5 @@
 public class diff {
     public static void main(String[] args) {
-        System.out.println("This is Diff.java class..");
+        System.out.println("This is Diff.java class");
     }
 }
